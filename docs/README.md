be2134316d80abdb17d453ce2923d964baa14b76
erha19 <faterrole@gmail.com>
2018-02-22 15:56:40 +0800

    first commit

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+# Introduction
+
+This boilerplate is targeted towards large, serious projects and assumes you are somewhat familiar with Webpack and `weex-loader`. 
+
+## Quickstart
+
+To use this template, scaffold a project with [weex-toolkit v1.3.0+](https://github.com/weex-team/weex-toolkit).
+
+``` bash
+$ npm install -g weex-toolkit
+$ weex create my-project # default will create the webpack template
+$ cd my-project
+$ npm install
+$ npm start
+```
---
+++ template/src/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo"></image>
+    <text class="greeting">The environment is ready!</text>
+    <text class="message">Now, let's use Vue.js to build your Weex app.</text>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
