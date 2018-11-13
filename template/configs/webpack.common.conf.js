f7c7d04d30b734be9e1c2b54df4a8ad723f5b988
erha19 <faterrole@gmail.com>
2018-11-13 17:58:44 +0800

    fixed apache/incubator-weex/issues/1686

--- template/configs/webpack.common.conf.js
+++ template/configs/webpack.common.conf.js
@@ -144,6 +144,17 @@ const useEslint = config.dev.useEslint ? [createLintingRule()] : []
  * Plugins for webpack configuration.
  */
 const plugins = [
+  /**
+   * Plugin: webpack.DefinePlugin
+   * Description: The DefinePlugin allows you to create global constants which can be configured at compile time. 
+   *
+   * See: https://webpack.js.org/plugins/define-plugin/
+   */
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': config.dev.env
+    }
+  }),
   /*
    * Plugin: BannerPlugin
    * Description: Adds a banner to the top of each generated chunk.
--- template/configs/webpack.release.conf.js
+++ template/configs/webpack.release.conf.js
@@ -182,6 +182,17 @@ const weexConfig = webpackMerge(commonConfig[1], {
    * See: http://webpack.github.io/docs/configuration.html#plugins
    */
   plugins: [
+    /**
+     * Plugin: webpack.DefinePlugin
+     * Description: The DefinePlugin allows you to create global constants which can be configured at compile time. 
+     *
+     * See: https://webpack.js.org/plugins/define-plugin/
+     */
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': config.prod.env
+      }
+    }),
     /*
      * Plugin: UglifyJsparallelPlugin
      * Description: Identical to standard uglify webpack plugin
