b3d37e9b2b67ac59b4f3d80ed56c18f52ec361f4
erha19 <faterrole@gmail.com>
2018-11-30 15:09:40 +0800

    [bugfix] resolve module from src

--- template/src/index.vue
+++ template/src/index.vue
@@ -12,7 +12,7 @@
 
 <script>
 {{#unless router}}
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 {{/unless}}
 export default {
   name: 'App',{{#router}}{{else}}
