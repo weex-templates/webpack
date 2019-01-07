95c03fa5c41cd8aaede531fd70e3e71b98593ff9
kuiwu <danwu.wdw@alibaba-inc.com>
2019-01-07 11:32:51 +0800

    Fixed weex-toolkit/issues/501

--- template/src/entry.js
+++ template/src/entry.js
@@ -9,7 +9,7 @@ weex.init(Vue);
 
 {{#router}}
 /* weex initialized here, please do not move this line */
-const router = require('./router');
+const { router } = require('./router');
 const App = require('@/index.vue');
 /* eslint-disable no-new */
 new Vue(Vue.util.extend({el: '#root', router}, App));
